8478bf80d98434a759ec9adaa8235bd73806e602
p88wen <108169495+p88wen@users.noreply.github.com>
2025-07-21 10:39:25 +0800

    Update aa.py

--- aa.py
+++ aa.py
@@ -54,7 +54,7 @@ class Spider:
                         title_match = re.search(r'title="([^"]+)"',  details["shortMessageHtmlLink"]) 
                         version = title_match.group(1).replace("Update to ", "") if title_match else None 
                         #downloadurl = f'https://gh-proxy.com/raw.githubusercontent.com/FongMi/Release/refs/heads/{own}/apk/release/{apk_name}'
-                        downloadurl = f'https://p88wen.github.io/{own}/apk/release/{apk_name}'               
+                        downloadurl = f'https://p88wen.github.io/{own}/{apk_name}'
                         # 构建APK信息对象 
                         apk_info = {
                             "name": apk_name_part,
